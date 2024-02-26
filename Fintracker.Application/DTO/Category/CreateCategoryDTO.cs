15e9722870373a980ff2d0bf678fcf1d9eb57c98
StepDad2002 <chermishan@gmail.com>
2024-02-26 22:24:19 +0200

    Refactor category create DTO

--- Fintracker.Application/DTO/Category/CreateCategoryDTO.cs
+++ Fintracker.Application/DTO/Category/CreateCategoryDTO.cs
@@ -3,10 +3,8 @@ using Fintracker.Application.DTO.Common;
 
 namespace Fintracker.Application.DTO.Category;
 
-public class CreateCategoryDTO : IBaseDto
+public class CreateCategoryDTO 
 {
-    public Guid Id { get; set; }
-    
     public string Name { get; set; }
 
     public CategoryTypeEnum Type { get; set; }
