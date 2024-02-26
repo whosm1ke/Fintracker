d82ffa86366d072c5abe14467ec0576c991247fc
StepDad2002 <chermishan@gmail.com>
2024-02-26 19:44:35 +0200

    Added custom exceptions

---
+++ Fintracker.Application/Exceptions/BadRequestException.cs
@@ -0,0 +1,9 @@
+﻿namespace Fintracker.Application.Exceptions;
+
+public class BadRequestException : ApplicationException
+{
+    public BadRequestException(string message) : base(message)
+    {
+        
+    }
+}
---
+++ Fintracker.Application/Exceptions/ValidationException.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation.Results;
+
+namespace Fintracker.Application.Exceptions;
+
+public class ValidationException : ApplicationException
+{
+    public List<string> Errors { get; set; } = new();
+
+    public ValidationException(ValidationResult validationResult)
+    {
+        validationResult.Errors.ForEach(x => Errors.Add(x.ErrorMessage));
+    }
+}
