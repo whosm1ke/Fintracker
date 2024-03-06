75ced1de65adf24818a5630654b3d86a86b7f6b2
StepDad2002 <chermishan@gmail.com>
2024-03-06 12:11:26 +0200

    Added UserController.cs

---
+++ Fintracker.API/Controllers/UserController.cs
@@ -0,0 +1,97 @@
+﻿using Fintracker.Application.DTO.User;
+using Fintracker.Application.Features.User.Requests.Commands;
+using Fintracker.Application.Features.User.Requests.Queries;
+using Fintracker.Application.Responses;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Fintracker.API.Controllers;
+
+[ApiController]
+[Route("api/user")]
+public class UserController : ControllerBase
+{
+    private readonly IMediator _mediator;
+
+    public UserController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    [HttpGet("{id:guid}")]
+    public async Task<ActionResult<UserBaseDTO>> Get(Guid id)
+    {
+        var response = await _mediator.Send(new GetUserByIdRequest()
+        {
+            Id = id
+        });
+
+        return Ok(response);
+    }
+    
+    [HttpGet("accessed-wallets/{walletId:guid}")]
+    public async Task<ActionResult<List<UserBaseDTO>>> GetWithAccessedWallets(Guid walletId)
+    {
+        var response = await _mediator.Send(new GetUsersAccessedToWalletRequest()
+        {
+            WalletId = walletId
+        });
+
+        return Ok(response);
+    }
+    
+    [HttpGet("{id:guid}/with-budgets")]
+    public async Task<ActionResult<UserWithBudgetsDTO>> GetWithBudgets(Guid id)
+    {
+        var response = await _mediator.Send(new GetUserWithBudgetsByIdRequest()
+        {
+            Id = id
+        });
+
+        return Ok(response);
+    }
+    
+    [HttpGet("{id:guid}/with-own-wallets")]
+    public async Task<ActionResult<UserWithOwnedWalletsDTO>> GetWithOwnWallets(Guid id)
+    {
+        var response = await _mediator.Send(new GetUserWithOwnedWalletsByIdRequest()
+        {
+            Id = id
+        });
+
+        return Ok(response);
+    }
+    
+    [HttpGet("{id:guid}/with-member-wallets")]
+    public async Task<ActionResult<UserWithMemberWalletsDTO>> GetWithMemberWallets(Guid id)
+    {
+        var response = await _mediator.Send(new GetUserWithMemberWalletsByIdRequest()
+        {
+            Id = id
+        });
+
+        return Ok(response);
+    }
+
+    [HttpDelete("{id:guid}")]
+    public async Task<ActionResult<DeleteCommandResponse<UserBaseDTO>>> Delete(Guid id)
+    {
+        var response = await _mediator.Send(new DeleteUserCommand()
+        {
+            Id = id
+        });
+
+        return Ok(response);
+    }
+
+    [HttpPut]
+    public async Task<ActionResult<UpdateCommandResponse<UserBaseDTO>>> Put([FromBody] UpdateUserDTO user)
+    {
+        var response = await _mediator.Send(new UpdateUserCommand()
+        {
+            User = user
+        });
+
+        return Ok(response);
+    }
+}
