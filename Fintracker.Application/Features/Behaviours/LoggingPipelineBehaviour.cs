3c99674f9aa1edbd2389869c558914b2fe5aa579
StepDad2002 <chermishan@gmail.com>
2024-03-15 00:58:52 +0200

    Fixed logging behaviour for requests

--- Fintracker.Application/Features/Behaviours/LoggingPipelineBehaviour.cs
+++ Fintracker.Application/Features/Behaviours/LoggingPipelineBehaviour.cs
@@ -6,7 +6,6 @@ namespace Fintracker.Application.Features.Behaviours;
 
 public class LoggingPipelineBehaviour<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
     where TRequest : IRequest<TResponse>
-    where TResponse : BaseCommandResponse
 {
 
     public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
@@ -15,7 +14,7 @@ public class LoggingPipelineBehaviour<TRequest, TResponse> : IPipelineBehavior<T
         Log.Information("Handling {Request}.", requestName);
         var response = await next();
         string responseName = typeof(TResponse).Name;
-        Log.Information("Handled {Response}. Success: {Success}.", responseName, response.Success);
+        Log.Information("Handled {Response}.", responseName);
         return response;
     }
 }
