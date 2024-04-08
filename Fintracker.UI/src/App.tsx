b0f478857b84399567645903952a12d26b3f453d
StepDad2002 <chermishan@gmail.com>
2024-04-08 16:37:37 +0300

    Added initial react project

---
+++ Fintracker.UI/src/App.tsx
@@ -0,0 +1,11 @@
+import './index.css'
+
+function App() {
+    return (
+        <>
+            <div className={"text-6xl font-bold"}>Hi there!</div>            
+        </>
+    )
+}
+
+export default App
---
+++ Fintracker.UI/vite.config.ts
@@ -0,0 +1,18 @@
+import {defineConfig} from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from 'tailwindcss'
+import mkcert from 'vite-plugin-mkcert'
+// https://vitejs.dev/config/
+export default defineConfig({
+    plugins: [react(), mkcert()],
+    server: {
+        https: true,
+        port: 1337
+    },
+    css:{
+        postcss:{
+            plugins: [tailwindcss]
+        }
+    },
+    
+})
